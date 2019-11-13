99550915c3b27e077003342bdcc563736e55d8ac
dr4nk <pedrompinheiro@hotmail.com>
2019-11-13 09:50:13 -0300

    Utilizando entrada por arquivo de texto.

--- GraphAdjList.py
+++ GraphAdjList.py
@@ -6,7 +6,10 @@ Project developed for the Algorithms and Data Structures II course in 2019
 Pourpose: To implement an algorithm to manipulate and show vertices and edges of a graph
 '''
 
-class Node:
+'''
+Vertex are the elements of a Graph
+'''
+class Vertex:
     def __init__(self, data=None):
         self.data = data
 
@@ -18,10 +21,28 @@ class Node:
     
     def set_next(self, newNext):
         self.next = newNext
-    
+
+
+'''
+Graph is a data structure of Vertices and a adjacency list or matrix
+'''
 class Graph:
     def __init__(self, root=None):
-        self.root = root
+        self.adjList = []
+
+    def showList(self):
+        for node in self.adjList:
+            while node != None:
+                print(node.val)
+
+'''
+Nodes are the LinkedList elements
+'''
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None #pointer initially points to nothing
+
 
 '''
 Function that creates an Node and adds a new index in the adjacency matrix
@@ -51,9 +72,22 @@ def ShowGraph():
 def IDSourceSink():
     pass
 
-nodes = []
+
+def CommandInput():    
+    newFile = open('entrada.txt', 'r')
+    fileLines = newFile.read().split('\n')
+    global fileData
+    fileData = []
+    for line in fileLines:
+        if line != '' and line[0] != '*':
+            fileData.append(line)
+
+CommandInput()
+print(fileData)
+
+'''nodes = []
 adjList = []
 nodes.append(InsertNode('ola mundo'))
 for i in range(0, len(nodes)):
     print(nodes[i])
-    print(adjList[i])
+    print(adjList[i])'''
