f261ace9ee1e184cac9591407380a8c2b765a607
dr4nk <pedrompinheiro@hotmail.com>
2019-11-13 15:09:00 -0300

    Inicio da funcao main e entrada tratada

--- GraphAdjList.py
+++ GraphAdjList.py
@@ -2,6 +2,7 @@
 Autor: Pedro Miranda Pinheiro
 Matricula: 128012
 
+
 Project developed for the Algorithms and Data Structures II course in 2019
 Pourpose: To implement an algorithm to manipulate and show vertices and edges of a graph
 '''
@@ -72,22 +73,38 @@ def ShowGraph():
 def IDSourceSink():
     pass
 
-
+'''
+This function opens, reads and closes the entrada.txt input file. 
+After reading it, it produces de fileCommands global list, wich contains
+a list of lists with commands and names for the instances of the classes.
+'''
 def CommandInput():    
     newFile = open('entrada.txt', 'r')
     fileLines = newFile.read().split('\n')
-    global fileData
-    fileData = []
-    for line in fileLines:
-        if line != '' and line[0] != '*':
-            fileData.append(line)
+    global fileCommands
+    fileCommands = []
+    i = 0
+    while i < len(fileLines):
+        if fileLines[i] != '' and fileLines[i][0] != '*':
+            fileCommands.append([fileLines[i], fileLines[i+1]])
+            i += 2
+        else:
+            i += 1
+    newFile.close()
 
-CommandInput()
-print(fileData)
+def main():
+    CommandInput()
+    print(fileCommands)
+    graphs = []
+    vertices = []
+    for command in fileCommands:
+        if command[0].lower() == 'graph':
+            graphs.append(Graph())
+        elif command[0].lower() == 'vertex':
+            vertices.append(Vertex(command[1]))
 
-'''nodes = []
-adjList = []
-nodes.append(InsertNode('ola mundo'))
-for i in range(0, len(nodes)):
-    print(nodes[i])
-    print(adjList[i])'''
+    print(graphs[0])
+    print(vertices[0])
+            
+
+main()
